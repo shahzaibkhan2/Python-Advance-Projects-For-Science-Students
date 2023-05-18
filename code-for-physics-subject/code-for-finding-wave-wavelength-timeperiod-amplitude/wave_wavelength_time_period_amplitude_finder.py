1101735b20c9c9c515ebc250ef7b39ad7fe9b8d6
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-17 22:11:38 -0700

    Code to Find waves wavelength,timeperiod,amplitude
    Code to Find waves wavelength,timeperiod,amplitude using Python

--- code-for-physics-subject/code-for-finding-wave-wavelength-timeperiod-amplitude/wave_wavelength_time_period_amplitude_finder.py
+++ code-for-physics-subject/code-for-finding-wave-wavelength-timeperiod-amplitude/wave_wavelength_time_period_amplitude_finder.py
@@ -1,1 +1,19 @@
+# Taking inputs
+v = float(input("Enter the velocity of the wave in m/s: "))
+f = float(input("Enter the frequency of the wave in Hz: "))
 
+# Calculating wavelength
+wavelength = v/f
+
+# Calculating time period
+time_period = 1/f
+
+# Displaying wavelength and time period
+print("The wavelength of the wave is {:.2f} m.".format(wavelength))
+print("The time period of the wave is {:.2f} s.".format(time_period))
+
+# Taking amplitude input
+amplitude = float(input("Enter the amplitude of the wave in m: "))
+
+# Displaying amplitude
+print("The amplitude of the wave is {:.2f} m.".format(amplitude))
