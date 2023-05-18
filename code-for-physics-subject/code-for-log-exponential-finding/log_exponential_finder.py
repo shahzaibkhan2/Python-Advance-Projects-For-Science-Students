8eaaec96d579af6fc1186be4392142998f7c977d
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-18 07:34:17 -0700

    Code to find Logarithm and exponential
    Code to find Logarithm and exponential using Python

--- code-for-physics-subject/code-for-log-exponential-finding/log_exponential_finder.py
+++ code-for-physics-subject/code-for-log-exponential-finding/log_exponential_finder.py
@@ -1,1 +1,20 @@
+# Import math module
+import math
 
+# Function for calculating logarithm
+def calculate_logarithm(base, num):
+    return math.log(num, base)
+
+# Function to find exponential
+def calculate_exponential(base, num):
+    return math.pow(base, num)
+
+# Example for usage
+base = 12
+num = 110
+
+logarithm_result = calculate_logarithm(base, num)
+exponential_result = calculate_exponential(base, num)
+
+print(f"The logarithm of {num} to the base {base} is: {logarithm_result}")
+print(f"The exponential of {num} to the base {base} is: {exponential_result}")
