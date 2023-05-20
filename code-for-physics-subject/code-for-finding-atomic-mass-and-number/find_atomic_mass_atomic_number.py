2a92b98ec117ab5e375e3e7d4a630f6fcc1a61d3
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-20 04:07:35 -0700

    Code to find atomic mass and number
    Code to find atomic mass and number using Python

--- code-for-physics-subject/code-for-finding-atomic-mass-and-number/find_atomic_mass_atomic_number.py
+++ code-for-physics-subject/code-for-finding-atomic-mass-and-number/find_atomic_mass_atomic_number.py
@@ -1,1 +1,21 @@
+# Import periodictable module
+from periodictable import elements
 
+# Function to get atomic properties like symbols,atomic masses and numbers
+def get_atomic_properties(element_symbol):
+    try:
+        element = elements.symbol(element_symbol)
+        atomic_number = element.number
+        atomic_mass = element.mass
+        return atomic_number, atomic_mass
+    except KeyError:
+        return None, None
+
+# Example for usage:
+symbol = input("Enter the symbol of an element: ")
+atomic_number, atomic_mass = get_atomic_properties(symbol)
+if atomic_number and atomic_mass:
+    print(f"Atomic number: {atomic_number}")
+    print(f"Atomic mass: {atomic_mass} g/mol")
+else:
+    print("Invalid element symbol!")
