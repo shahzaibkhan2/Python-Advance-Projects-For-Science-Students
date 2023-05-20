d4806ef6a3d1ee331600195d9b7ee3da44f74dcf
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-05-20 03:47:26 -0700

    Code to find velocity time graph
    Code to find velocity time graph using Python

--- code-for-physics-subject/code-for-velocity-time-graph/velocity_time_graph_finder.py
+++ code-for-physics-subject/code-for-velocity-time-graph/velocity_time_graph_finder.py
@@ -1,1 +1,17 @@
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
+# Function for plotting velocity time graph
+def plot_velocity_time_graph(time, velocity):
+    plt.plot(time, velocity)
+    plt.title('Velocity-Time Graph')
+    plt.xlabel('Time')
+    plt.ylabel('Velocity')
+    plt.show()
+
+# Example for data
+time = [0, 1, 4, 3, 5, 7]  # Time values
+velocity = [0, 11, 21, 25, 5, 10]  # Velocity values
+
+# Generate velocity-time graph
+plot_velocity_time_graph(time, velocity)
