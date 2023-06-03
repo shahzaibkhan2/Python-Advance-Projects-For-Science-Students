0e3fda9e1f8223f97dd749a127554df8446a1364
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-06-03 08:56:07 -0700

    Code for Cramer's rule
    Code for Cramer's rule using Python

--- codes-for-math-subject/code-for-cramer's-rule/cramers_rule.py
+++ codes-for-math-subject/code-for-cramer's-rule/cramers_rule.py
@@ -1,1 +1,37 @@
+# Import numpy module
+import numpy as np
 
+# Function to solve cramer's problem
+def cramer_solver(coefficients, constants):
+    """
+    Solves a system of linear equations using Cramer's rule.
+    :param coefficients: 2D array representing the coefficients of the variables.
+    :param constants: 1D array representing the constants.
+    :return: 1D array of solutions for each variable.
+    """
+    det_a = np.linalg.det(coefficients)
+
+    if det_a == 0:
+        raise ValueError("The system of equations does not have a unique solution.")
+
+    num_variables = len(coefficients[0])
+    solutions = []
+
+    for i in range(num_variables):
+        submatrix = np.copy(coefficients)
+        submatrix[:, i] = constants
+        det_submatrix = np.linalg.det(submatrix)
+        solutions.append(det_submatrix / det_a)
+
+    return solutions
+
+# Example for usage:
+temp_coefficients = np.array([[2, 1, -1],
+                         [3, 4, 2],
+                         [1, -3, 5]])
+
+temp_constants = np.array([4, 10, 12])
+
+temp_solutions = cramer_solver(temp_coefficients, temp_constants)
+
+print("Solution:", temp_solutions)
