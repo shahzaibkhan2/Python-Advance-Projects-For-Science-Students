2610d5a1f74025f331ef5f49a665aae2fb90faf4
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-06-04 06:35:12 -0700

    Code to identify chemical formulae for medicine
    Code to identify chemical formulae for medicine using Python

--- codes-for-biology-subject/code-to-identify-chemical-formulae-for-medicine/indentifying_medicine_formula.py
+++ codes-for-biology-subject/code-to-identify-chemical-formulae-for-medicine/indentifying_medicine_formula.py
@@ -1,1 +1,32 @@
+# Function for parsing chemical formula
+def parse_chemical_formula(formula):
+    elements = {}
+    current_element = ""
+    current_count = ""
+    for char in formula:
+        if char.isalpha():
+            if current_element != "":
+                if current_count == "":
+                    current_count = "1"
+                elements[current_element] = elements.get(current_element, 0) + int(current_count)
+                current_count = ""
+            current_element = char
+        elif char.isdigit():
+            current_count += char
+    if current_element != "":
+        if current_count == "":
+            current_count = "1"
+        elements[current_element] = elements.get(current_element, 0) + int(current_count)
+    return elements
 
+# Function to execute main object
+def main():
+    formula = input("Enter the chemical formula: ")
+    elements = parse_chemical_formula(formula)
+    print("Chemical formula breakdown:")
+    for element, count in elements.items():
+        print(f"{element}: {count}")
+
+# Main object
+if __name__ == "__main__":
+    main()
