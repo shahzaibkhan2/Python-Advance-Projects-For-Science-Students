4e688324bb706a85fcaf41760606e14d1d6e8eb4
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-06-04 06:40:20 -0700

    Code for venn diagrams
    Code for venn diagrams using Python

--- codes-for-math-subject/code-for-venn-diagrams/venn_diagrams.py
+++ codes-for-math-subject/code-for-venn-diagrams/venn_diagrams.py
@@ -1,1 +1,20 @@
+# Import necessary modules
+import matplotlib.pyplot as plt
+from matplotlib_venn import venn3
 
+# Define the sets
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7}
+set3 = {5, 6, 7, 8, 9}
+
+# Create the Venn diagram
+venn = venn3([set1, set2, set3], set_labels=('Set 1', 'Set 2', 'Set 3'))
+
+# Customize the diagram and its optional
+venn.get_label_by_id('100').set_text('Set 1')
+venn.get_label_by_id('010').set_text('Set 2')
+venn.get_label_by_id('001').set_text('Set 3')
+
+# Display the diagram
+plt.title("Venn Diagram")
+plt.show()
