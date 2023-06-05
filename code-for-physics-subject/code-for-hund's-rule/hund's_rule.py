131db9db531faf6f784bdb30676f42fd1af8b30b
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-06-05 05:57:46 -0700

    Code for Hund's rule
    Code for Hund's rule using Python

--- code-for-physics-subject/code-for-hund's-rule/hund's_rule.py
+++ code-for-physics-subject/code-for-hund's-rule/hund's_rule.py
@@ -1,1 +1,16 @@
+# Function to find number of electrons according to hund's rule
+def hunds_rule(num_electrons):
+    orbitals = []
+    electrons_left = num_electrons
+
+    while electrons_left > 0:
+        max_electrons = min(2, electrons_left)
+        orbitals.append(max_electrons)
+        electrons_left -= max_electrons
+
+    return orbitals
+# Example for usage:
+num_electrons = 10
+orbital_config = hunds_rule(num_electrons)
+print(f"Orbital configuration for {num_electrons} electrons: {orbital_config}")
 
