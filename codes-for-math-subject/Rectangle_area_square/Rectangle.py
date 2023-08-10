072620ed9c986c33d126218ed8216ba43559f790
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-08-09 21:38:13 -0700

    Rectangle
    Rectangle

--- codes-for-math-subject/Rectangle_area_square/Rectangle.py
+++ codes-for-math-subject/Rectangle_area_square/Rectangle.py
@@ -1,1 +1,21 @@
+# RECTANGLE
 
+class Rectangle:
+    def __init__(self, l, b):
+        self.length = l
+        self.breadth = b
+        
+    @classmethod
+    def property(cls, leng, bread):
+        return cls(leng, bread)
+    
+    def area(self):
+        return self.length * self.breadth
+    
+    def is_square(self):
+        return True if self.length == self.breadth else False
+    
+    
+rec = Rectangle.property(4, 4)
+print(rec.area())
+print(rec.is_square())
